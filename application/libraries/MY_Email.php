3ee561d830583c234d0e2fd9b8cd6b0768967bc7
truejay <jay@truejay.com>
2010-09-29 13:11:47 -0700

    Bug fix

--- application/libraries/MY_Email.php
+++ application/libraries/MY_Email.php
@@ -8,16 +8,16 @@ class MY_Email extends CI_Email
         
         $config['protocol'] = $ci->config->item('email_protocol');
         $config['smtp_host'] = $ci->config->item('email_smtp_host');
-        $config['smtp_port'] = $ci->config->item('email_port');
-        $config['smtp_timeout'] = $ci->config->item('email_timeout');
-        $config['smtp_user'] = $ci->config->item('email_user');
-        $config['smtp_pass'] = $ci->config->item('email_pass');
+        $config['smtp_port'] = $ci->config->item('email_smtp_port');
+        $config['smtp_timeout'] = $ci->config->item('email_smtp_timeout');
+        $config['smtp_user'] = $ci->config->item('email_smtp_user');
+        $config['smtp_pass'] = $ci->config->item('email_smtp_pass');
         $config['newline'] = $ci->config->item('email_newline');
         $config['mailtype'] = $ci->config->item('email_mailtype');
         $config['validation'] = $ci->config->item('email_validation');
-        
+
         $this->initialize($config);
-        $this->from($ci->config->item('email_user'), $ci->config->item('website_name'));
+        $this->from($ci->config->item('email_smtp_user'), $ci->config->item('website_name'));
         $this->to($to); 
         $this->subject($subject);
         $this->message($message);  
